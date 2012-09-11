1a1223b04a7b97edad882661b3eb97b8042d2d91
Moritz Schlarb <mail@moritz-schlarb.de>
2012-09-11 12:32:31 +0200

    Version bump to 0.2

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 libripoff
 =========
 
-simple library to detect plagiarism in source code
+A simple library to detect plagiarism in source code and cluster the
+results for convenient detection.
+
+For documentation please see the comments in the source files, the
+examples in the examples/ directory and the test suite in the tests/
+directory.
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+'''setup file for libripoff
+
+@author: moschlar
+'''
 from setuptools import setup
 
 setup(
     name = "libripoff",
-    version = "0.1",
+    version = "0.2",
     author = "Christian Hundt, Moritz Schlarb",
     author_email = "hundt.christian@gmail.com, mail@moritz-schlarb.de",
     description = "simple library to detect plagiarism in source code",
+    long_description = open('README.md').read(),
     license = "BSD",
     url = "https://github.com/gravitino/libripoff",
     packages = ["ripoff"],
     classifiers = [
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Education",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries",
