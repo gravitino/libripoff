384786b87f5e790de7102d5ec57e4f00d6c21329
Moritz Schlarb <mail@moritz-schlarb.de>
2012-09-11 13:01:16 +0200

    pep8 in setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,30 @@
 '''
 from setuptools import setup
 
+# Fix shutdown errors while installing
+try:
+    import multiprocessing  # @UnusedImport
+    import logging  # @UnusedImport
+except:
+    pass
+
 setup(
-    name = "libripoff",
-    version = "0.2",
-    author = "Christian Hundt, Moritz Schlarb",
-    author_email = "hundt.christian@gmail.com, mail@moritz-schlarb.de",
-    description = "simple library to detect plagiarism in source code",
-    long_description = open('README.md').read(),
-    license = "BSD",
-    url = "https://github.com/gravitino/libripoff",
-    packages = ["ripoff"],
+    name="libripoff",
+    version="0.2",
+    author="Christian Hundt, Moritz Schlarb",
+    author_email="hundt.christian@gmail.com, mail@moritz-schlarb.de",
+    description="simple library to detect plagiarism in source code",
+    long_description=open('README.md').read(),
+    license="BSD",
+    url="https://github.com/gravitino/libripoff",
+    packages=["ripoff"],
     test_suite='tests',
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Education",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: BSD License",
     ],
-    install_requires = ['numpy', 'hcluster', 'matplotlib'],
+    install_requires=['numpy', 'hcluster', 'matplotlib'],
 )
